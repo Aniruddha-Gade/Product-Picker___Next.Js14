2fccf53a5b99611062820cd1e2803852ac7ab1f4
Aniruddha Gade <gadeaniruddha2@gmail.com>
2024-08-25 23:00:09 +0530

    setup: backend with Express.Js, TypeScript , mongoose, jsonwebtoken,  and other libraries

---
+++ server/app.ts
@@ -0,0 +1,52 @@
+import express, { NextFunction, Request, Response } from "express";
+import cors from "cors";
+import cookieParser from "cookie-parser";
+
+require("dotenv").config();
+
+export const app = express();
+
+// body parser
+app.use(express.json({ limit: "50mb" }));
+// cookie parser
+app.use(cookieParser());
+// cors => cors
+app.use(
+    cors({
+        origin: process.env.ORIGIN,
+    })
+);
+
+
+
+
+
+
+// Default Route
+app.get('/', (req: Request, res: Response,) => {
+    // console.log('Your server is up and running..!');
+    res.send(`<div>
+    <h1>Product Picker Backend</h1>
+    <p>This is Default Route</p>
+    <p>Everything is OK</p>
+    </div>`);
+})
+
+
+// testing route
+app.get('/test', (req: Request, res: Response, next: NextFunction) => {
+    res.status(200).json({
+        success: true,
+        message: 'API is working'
+    })
+})
+
+
+// unknown route
+app.all('*', (req: Request, res: Response, next: NextFunction) => {
+    const error = new Error(`Route ${req.originalUrl} not found`) as any
+    error.statusCode = 404
+    next(error)
+})
+
+
