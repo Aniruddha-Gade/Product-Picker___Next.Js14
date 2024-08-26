5aed5b83111d25c64a357fc1de893acf48bf9698
Aniruddha Gade <gadeaniruddha2@gmail.com>
2024-08-26 13:20:53 +0530

    feat: add isAuthenticated middleware to secure user logout

--- server/route/user.route.ts
+++ server/route/user.route.ts
@@ -1,12 +1,13 @@
 import { Router } from "express";
 import { registerUser, activateUser, loginUser, logoutUser } from './../controllers/user.controller';
+import { isAuthenticated } from "../middleware/auth";
 
 const userRouter = Router()
 
 userRouter.post('/registration', registerUser)
 userRouter.post('/activate-user', activateUser)
 userRouter.post('/login', loginUser)
-userRouter.get('/logout', logoutUser)
+userRouter.get('/logout', isAuthenticated, logoutUser)
 
 
 
