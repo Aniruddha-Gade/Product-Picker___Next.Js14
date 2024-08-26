77e369f01546aed8a8fb6d829fa840193408e142
Aniruddha Gade <gadeaniruddha2@gmail.com>
2024-08-26 13:38:01 +0530

    add: Review Model

---
+++ server/models/review.model.ts
@@ -0,0 +1,42 @@
+import { Schema, model, models } from 'mongoose';
+import { IProduct } from './product.model';
+
+
+interface IReview {
+    productId: Schema.Types.ObjectId;
+    updatedFields: Partial<IProduct>;
+    status: 'pending' | 'approved' | 'rejected';
+    submittedBy: Schema.Types.ObjectId;
+    reviewedBy?: Schema.Types.ObjectId;   // mark as optional, initially the review is not reviewed
+}
+
+
+const reviewSchema = new Schema<IReview>({
+    productId: {
+        type: Schema.Types.ObjectId,
+        ref: 'Product',
+        required: true
+    },
+    updatedFields: {
+        type: Schema.Types.Mixed,
+        required: true
+    },
+    status: {
+        type: String,
+        enum: ['pending', 'approved', 'rejected'],
+        default: 'pending'
+    },
+    submittedBy: {
+        type: Schema.Types.ObjectId,
+        ref: 'User',
+        required: true
+    },
+    reviewedBy: {
+        type: Schema.Types.ObjectId,
+        ref: 'User'
+    },
+});
+
+
+const ReviewModel = models.Review || model<IReview>('Review', reviewSchema);
+export default ReviewModel;
