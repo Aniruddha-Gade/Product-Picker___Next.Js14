8c4f5ad5ec4b325950f5f4906463529cfbcba09c
Aniruddha Gade <gadeaniruddha2@gmail.com>
2024-08-26 13:33:12 +0530

    add: Product Model

---
+++ server/models/product.model.ts
@@ -0,0 +1,39 @@
+import { Schema, model, models } from 'mongoose';
+
+export interface IProduct {
+    title: string;
+    description: string;
+    images: string[];
+    price: number;
+    createdBy: Schema.Types.ObjectId;
+}
+
+
+const productSchema = new Schema<IProduct>({
+    title: {
+        type: String,
+        required: true
+    },
+    description: {
+        type: String,
+        required: true
+    },
+    images: {
+        type: [String],
+        required: true
+    },
+    price: {
+        type: Number,
+        required: true
+    },
+    createdBy: {
+        type: Schema.Types.ObjectId,
+        ref: 'User',
+        required: true
+    },
+});
+
+
+
+const ProductModel = models.Product || model<IProduct>('Product', productSchema);
+export default ProductModel;
