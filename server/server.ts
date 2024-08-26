a175a7340a97c6ccf4011087bc7a85f0f1323196
Aniruddha Gade <gadeaniruddha2@gmail.com>
2024-08-26 10:52:01 +0530

    connect mongodb database to backend

--- server/server.ts
+++ server/server.ts
@@ -1,4 +1,5 @@
 import { app } from "./app";
+import connectDB from "./utils/database";
 
 require('dotenv').config()
 
@@ -13,5 +14,5 @@ const PORT = process.env.PORT || 5000
 app.listen(PORT, () => {
     console.log("=================================")
     console.log(`Server started on PORT ${PORT}`)
-   
+    connectDB()
 })
