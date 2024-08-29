a2da04fdb36bead08b62e46358e7510be688c789
Aniruddha Gade <gadeaniruddha2@gmail.com>
2024-08-29 16:01:43 +0530

    update list of pending requests

--- client/app/dashboard/admin/pending-requests/page.tsx
+++ client/app/dashboard/admin/pending-requests/page.tsx
@@ -18,7 +18,7 @@ const PendingRequestPage = () => {
 
   useEffect(() => {
     if (isSuccess) {
-      toast.success("All Products fetched Successfully")
+      toast.success("All pending requests fetched Successfully")
       setPendingRequests(data.pendingReviews)
     }
     if (error) {
@@ -31,68 +31,67 @@ const PendingRequestPage = () => {
 
   return (
     <SidebarLayout userRole={user?.accountType}>
-        <AdminProtected>
-      <div className='min-h-screen flex-col w-full text-black dark:text-white p-5'>
-        <h1 className="text-2xl font-bold mb-6">All Pending Requests</h1>
+      <AdminProtected>
+        <div className='min-h-screen flex-col w-full text-black dark:text-white p-5'>
+          <h1 className="text-2xl font-bold mb-6">All Pending Requests</h1>
 
 
-        {pendingRequests && pendingRequests.length > 0 ? (
-        <ul className="space-y-4">
-          {pendingRequests.map((request) => (
-            <Link
-              key={request._id}
-              href={`/product/${request.productId._id}`}
-              className="block p-5 bg-white dark:bg-gray-800 shadow-md hover:shadow-lg rounded-lg transition-shadow duration-300"
-            >
-              <li className="flex flex-col md:flex-row justify-between items-start md:items-center">
-                <div className="mb-4 md:mb-0">
-                  <h3 className="text-lg font-semibold text-gray-900 dark:text-white">
-                    {request.productId.title}
-                  </h3>
-                  <p className="text-sm text-gray-600 dark:text-gray-400">
-                    Submitted by: <span className="font-medium text-gray-800 dark:text-gray-200">{request.submittedBy.name}</span>
-                  </p>
-                  <p className="text-sm text-gray-600 dark:text-gray-400">
-                    Email: <span className="font-medium text-gray-800 dark:text-gray-200">{request.submittedBy.email}</span>
-                  </p>
-                </div>
+          {pendingRequests && pendingRequests.length > 0 ? (
+            <ul className="space-y-4">
+              {pendingRequests.map((request) => (
+                <Link
+                  key={request._id}
+                  href={`/pending-requests/${request._id}`}
+                  className="block p-5 bg-white dark:bg-gray-800 shadow-md hover:shadow-lg rounded-lg transition-shadow duration-300"
+                >
+                  <li className="flex flex-col md:flex-row justify-between items-start md:items-center">
+                    <div className="mb-4 md:mb-0">
+                      <h3 className="text-lg font-semibold text-gray-900 dark:text-white">
+                        {request.productId.title}
+                      </h3>
+                      <p className="text-sm text-gray-600 dark:text-gray-400">
+                        Submitted by: <span className="font-medium text-gray-800 dark:text-gray-200">{request.submittedBy.name}</span>
+                      </p>
+                      <p className="text-sm text-gray-600 dark:text-gray-400">
+                        Email: <span className="font-medium text-gray-800 dark:text-gray-200">{request.submittedBy.email}</span>
+                      </p>
+                    </div>
 
-                <div className='flex gap-5 items-center '>
-                <div>
-                 <p className="text-sm text-gray-600 dark:text-gray-400">
-                  Reviewed by: <span className="font-medium text-gray-800 dark:text-gray-200">{request.reviewedBy.name|| 'Not yet reviewed'}</span>
-                  </p>
-                  <p className="text-sm text-gray-600 dark:text-gray-400">
-                    Email: <span className="font-medium text-gray-800 dark:text-gray-200">{request.reviewedBy.email|| 'Not yet reviewed'}</span>
-                  </p>
-                 </div>
+                    <div className='flex gap-5 items-center '>
+                      <div>
+                        <p className="text-sm text-gray-600 dark:text-gray-400">
+                          Reviewed by: <span className="font-medium text-gray-800 dark:text-gray-200">{request.reviewedBy?.name || 'Not yet reviewed'}</span>
+                        </p>
+                        <p className="text-sm text-gray-600 dark:text-gray-400">
+                          Email: <span className="font-medium text-gray-800 dark:text-gray-200">{request.reviewedBy?.email || 'Not yet reviewed'}</span>
+                        </p>
+                      </div>
 
-                <div className="flex items-center space-x-2">
-                  <span className={`inline-block px-3 py-1 rounded-full text-sm font-medium ${
-                    request.status === 'pending'
-                      ? 'bg-yellow-200 text-yellow-800'
-                      : request.status === 'approved'
-                      ? 'bg-green-200 text-green-800'
-                      : 'bg-red-200 text-red-800'
-                  }`}>
-                    {request.status}
-                  </span>
+                      <div className="flex items-center space-x-2">
+                        <span className={`inline-block px-3 py-1 rounded-full text-sm font-medium ${request.status === 'pending'
+                            ? 'bg-yellow-200 text-yellow-800'
+                            : request.status === 'approved'
+                              ? 'bg-green-200 text-green-800'
+                              : 'bg-red-200 text-red-800'
+                          }`}>
+                          {request.status}
+                        </span>
 
 
-                </div>
-                </div>
-              </li>
-            </Link>
-          ))}
-        </ul>
-      ) : (
-        <div className="text-center text-gray-600 dark:text-gray-400">
-          No pending requests found.
+                      </div>
+                    </div>
+                  </li>
+                </Link>
+              ))}
+            </ul>
+          ) : (
+            <div className="text-center text-gray-600 dark:text-gray-400">
+              No pending requests found.
+            </div>
+          )}
+
+
         </div>
-      )}
-
-       
-      </div>
       </AdminProtected>
     </SidebarLayout>
   );
