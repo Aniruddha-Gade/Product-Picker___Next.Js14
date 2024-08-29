0c1be5c87be4492c42763f086e8e30b9f56208a7
Aniruddha Gade <gadeaniruddha2@gmail.com>
2024-08-29 16:03:25 +0530

    feat: add constants for ACCOUNT_TYPE to define user roles (Team Member, Admin)

--- client/app/hooks/adminProtected.tsx
+++ client/app/hooks/adminProtected.tsx
@@ -1,6 +1,8 @@
 import { redirect } from "next/navigation";
 import React from "react";
 import { useSelector } from "react-redux";
+import {ACCOUNT_TYPE} from '../constants/account-types'
+
 
 interface ProtectedProps {
   children: React.ReactNode;
@@ -9,7 +11,7 @@ interface ProtectedProps {
 export default function AdminProtected({ children }: ProtectedProps) {
   const { user } = useSelector((state: any) => state.auth);
 
-  const isAdmin = user?.accountType === "Admin";
+  const isAdmin = user?.accountType ===  ACCOUNT_TYPE.ADMIN;
 
   return isAdmin ? children : redirect("/");
 }
