36370decac76f3671a4dc5ad333655e9410d31f7
Aniruddha Gade <gadeaniruddha2@gmail.com>
2024-08-29 16:02:06 +0530

    update list of all products

--- client/app/dashboard/all-products/page.tsx
+++ client/app/dashboard/all-products/page.tsx
@@ -6,15 +6,9 @@ import { toast } from 'sonner';
 import SidebarLayout from '../../components/sidebar/SidebarLayout'
 import { useAllProductsQuery } from '../../redux/features/product/productApi';
 import Link from "next/link"
+import {IProduct} from "../../types/type"
 
 
-interface Product {
-  _id: string;
-  title: string;
-  description: string;
-  price: number;
-  status: 'pending' | 'approved' | 'rejected';
-}
 
 
 const AllProductsPage = () => {
@@ -44,28 +38,28 @@ const AllProductsPage = () => {
 
 
         <ul className="w-full grid grid-rows-4 grid-flow-col gap-4">
-        {products.map((product: Product) => (
+        {products.map((product: IProduct) => (
             <Link
               href={`/product/${product._id}`}
               className="p-4  bg-black/5 dark:bg-white/10 hover:bg-black/10 hover:dark:bg-white/15 rounded-xl hover:shadow-md"
             >
               <li key={product._id} className='flex justify-between ' >
                <div>
-               <h2 className="text-xl font-semibold">{product.title}</h2>
-                <p>{product.description}</p>
-                <p className="font-bold">Price: ${product.price}</p>
+               <h2 className="text-xl font-semibold">{product?.title}</h2>
+                <p>{product?.description}</p>
+                <p className="font-bold">Price: ${product?.price}</p>
                </div>
 
                 <div className="flex flex-col items-center gap-2">
-                  <span className={`inline-block px-3 py-1 rounded-full text-sm font-medium ${
-                    product.status === 'pending'
+                  {/* <span className={`inline-block px-3 py-1 rounded-full text-sm font-medium ${
+                    product?.status === 'pending'
                       ? 'bg-yellow-200 text-yellow-800'
                       : product.status === 'approved'
                       ? 'bg-green-200 text-green-800'
                       : 'bg-red-200 text-red-800'
                   }`}>
                     {product.status}
-                  </span>
+                  </span> */}
 
                 </div>
               </li>
