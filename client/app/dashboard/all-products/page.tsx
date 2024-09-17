9d7596adef86857e918237cc86fc6168af6f733e
Aniruddha Gade <gadeaniruddha2@gmail.com>
2024-09-17 16:33:48 +0530

    add Product resuable component

--- client/app/dashboard/all-products/page.tsx
+++ client/app/dashboard/all-products/page.tsx
@@ -5,11 +5,9 @@ import { useSelector } from 'react-redux'
 import { toast } from 'sonner';
 import SidebarLayout from '../../components/sidebar/SidebarLayout'
 import { useAllProductsQuery } from '../../redux/features/product/productApi';
-import Link from "next/link"
-import Image from "next/image"
 import { IProduct } from "../../types/type"
 import { LoadingProductSkeleton } from "../../utils/LoadingSkeleton"
-import { formatDate } from '../../../lib/formatDate'
+import Product from '../../components/product/product'
 
 import {
   Pagination,
@@ -107,46 +105,10 @@ const AllProductsWithPaginationPage = () => {
 
             <ul className="w-full grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 2xl:grid-cols-4 gap-4">
               {products && products?.map((product: IProduct) => (
-                <Link
-                  key={product._id}
-                  href={`/product/${product._id}`}
-                  className=" p-4 bg-black/5 dark:bg-white/10 hover:bg-black/10 hover:dark:bg-white/15 rounded-xl hover:shadow-md"
-                >
-                  <div className='w-full h-60 flex-center '>
-                    <Image
-                      src={product?.image ? product?.image : '/assets/images/not-available.jpg'}
-                      width={230}
-                      height={230}
-                      className='w-full h-full object-cover rounded-xl '
-                      alt={`${product?.title}`}
-                    />
-                  </div>
-
-                  <li className="flex flex-col gap-3 justify-between mt-5">
-                    <div className='flex flex-col gap-2'>
-                      <h2 className="text-xl font-semibold">{product?.title}</h2>
-                      <p>{product?.description}</p>
-                      <p className="font-bold">Price: ₹{product?.price}</p>
-                      <p className="text-sm text-gray-600 dark:text-gray-400">
-                        Date: <span className="font-medium text-gray-800 dark:text-gray-200">
-                          {product?.createdAt ? formatDate(product?.createdAt) : 'Date not available'}
-                        </span>
-                      </p>
-                    </div>
-                    <div>
-                      <span className={`inline-block px-3 py-1 rounded-full text-sm font-medium ${product?.status === 'pending'
-                        ? 'bg-yellow-200 text-yellow-800'
-                        : product.status === 'approved'
-                          ? 'bg-green-200 text-green-800'
-                          : 'bg-red-200 text-red-800'
-                        }`}>
-                        {product?.status}
-                      </span>
-                    </div>
-                  </li>
-                </Link>
+                 <Product key={product._id} product={product} />
               ))}
             </ul>
+
 
             {/* Pagination */}
             <Pagination className='mt-10'>
